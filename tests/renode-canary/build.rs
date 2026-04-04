4645385200dc7ebbe28e8857cc5d23aabe0b333a
Mike Avine <mike.avine@gmail.com>
2026-04-03 21:45:48 -0700

    fix canary

--- tests/renode-canary/build.rs
+++ tests/renode-canary/build.rs
@@ -4,16 +4,18 @@ use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    // Put the linker script in the build directory
+    // Put `memory.x` in our output directory and ensure it's
+    // on the linker search path.
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out.join("memory.x"))
         .unwrap()
         .write_all(include_bytes!("memory.x"))
         .unwrap();
-
-    // Tell Cargo where to find the linker script
     println!("cargo:rustc-link-search={}", out.display());
 
-    // Only re-run if memory.x changes
     println!("cargo:rerun-if-changed=memory.x");
+
+    // These flags are required for the RP2350 target
+    println!("cargo:rustc-link-arg-bins=--nmagic");
+    println!("cargo:rustc-link-arg-bins=-Tlink.x");
 }
